a5304bf2bfc5789a4220a71dd1255c131c8fa37b
Jenny Bao <jennybao1004@gmail.com>
2023-09-10 20:06:12 -0400

    [WIP] add geojson map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import GeoJSONMap from './components/GeoJSONMap';
 import './App.css';
+import ShapefileMap from './components/ShapefileMap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GeoJSONMap />
+      <ShapefileMap />
     </div>
   );
 }
