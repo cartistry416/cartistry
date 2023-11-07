2d6a84f0da139cd8453300a24a0cc2de28c29060
Jwaibong <jonathan.ng@stonybrook.edu>
2023-11-07 17:30:51 -0500

    running front end tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,14 +1,15 @@
-
+// comment for github actions
 describe('Dummy Test', () => {
   it('Dummy button', () => {
-      cy.visit('http://localhost:3000/');
+      cy.visit('https://main.d2cpsfn3mxqyu2.amplifyapp.com/');
       cy.get('#dummy').click()
       cy.get('#dummyText').should('exist').should('have.text', JSON.stringify('dummyData'))
   });
 
   it('Email Form', () => {
-      cy.visit('http://localhost:3000/');
+      cy.visit('https://main.d2cpsfn3mxqyu2.amplifyapp.com/');
       cy.get('#email').type('McKillaGorilla').should('have.value', 'McKillaGorilla')
+
   })
 });
 
