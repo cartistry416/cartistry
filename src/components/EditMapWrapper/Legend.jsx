75f946f4e184e0a150b82d910d55fd1a230bdea9
Angelo Panopio <angelo.panopio@stonybrook.edu>
2023-11-13 00:57:05 -0500

    css for edit map toolbox and legend

--- src/components/EditMapWrapper/Legend.jsx
+++ src/components/EditMapWrapper/Legend.jsx
@@ -1,18 +1,21 @@
 const Legend = () => {
     return (
       <div className="legend">
-        <div className="imagePreviewContainer">
-          {/* Image Preview would be a component or an img tag depending on your implementation */}
-          <div className="imagePreview">Image Preview</div>
+        <div className="legend-header">
+          <h2 className="legend-title">Legend</h2>
         </div>
-        <div className="textLabelContainer">
-          <span className="textLabel">Text Label</span>
+        <div className="legend-body">
+            <div className="imagePreviewContainer">
+                <div className="imagePreview">Image Preview</div>
+            </div>
+            <div className="textLabelContainer">
+                <span className="textLabel">Text Label</span>
+            </div>
+            <div className="labelOneContainer">
+                <span className="labelOne">Label One</span>
+            </div>
         </div>
-        {/* Additional labels as required */}
-        <div className="labelOneContainer">
-          <span className="labelOne">Label One</span>
-        </div>
-        {/* ... other legend items */}
+       
       </div>
     );
   };
--- src/components/EditMapWrapper/Toolbox.jsx
+++ src/components/EditMapWrapper/Toolbox.jsx
@@ -1,20 +1,26 @@
 const Toolbox = () => {
     return (
       <div className="toolbox">
-        <button className="toolButton pan">Pan</button>
-        <button className="toolButton fill">Fill</button>
-        <button className="toolButton point">Point</button>
+        <div className="toolbox-header">
+          <h2 className="map-title">Map Title</h2>
+          <button className="menu-button">☰</button>
+        </div>
+        <div className="toolbox-body">
+            <button className="toolButton pan">Pan</button>
+            <button className="toolButton fill">Fill</button>
+            <button className="toolButton point">Point</button>
 
+            
+            <button className="toolButton gradient">Gradient</button>
+            <button className="toolButton line">Line</button>
+            <button className="toolButton polygon">Polygon</button>
+            
+            <button className="toolButton landmark">Landmark</button>
+            <input type="text" placeholder="Label" className="textInput" />
+            <input type="color" className="colorInput" />
+        </div>
         
-        <button className="toolButton gradient">Gradient</button>
-        <button className="toolButton line">Line</button>
-        <button className="toolButton polygon">Polygon</button>
-        
-        <input type="text" placeholder="Label" className="textInput" />
-        <input type="color" className="colorInput" />
 
-        <button className="toolButton landmark">Landmark</button>
-        {/* ... other tools */}
       </div>
     );
   };
