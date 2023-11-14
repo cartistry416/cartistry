b4e3ebe3d86be302f5e7073496ce5920c967555f
Jenny Bao <jennybao1004@gmail.com>
2023-11-14 15:56:52 -0500

    add account dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "../static/css/navBar.css";
 
 function NavBar() {
+  const [showDropdown, setShowDropdown] = useState(false)
   return (
     <div id="navBarWrapper">
       <h1 id="logotype">Cartistry</h1>
-      <div class="profileIcon material-icons">account_circle</div>
+      <div className="profileIcon material-icons" onClick={() => setShowDropdown(!showDropdown)}>account_circle</div>
+      {showDropdown && (
+        <div className="nav-dropdown">
+          <div className="nav-dropdown-list">
+            <div className="nav-dropdown-option">
+              <span>My Maps</span>
+            </div>
+            <div className="nav-dropdown-option">
+              <span>My Posts</span>
+            </div>
+            <div className="nav-dropdown-option">
+              <span>Reset Password</span>
+            </div>
+            <div className="nav-dropdown-option">
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
