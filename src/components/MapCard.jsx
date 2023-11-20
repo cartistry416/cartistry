5ac1cd4cf6cdd0347a7dbf4488a502a8281e186e
us1aaron <aaron.li@stonybrook.edu>
2023-11-20 18:25:37 -0500

    stop propagation, redirect when clicking on map card

--- src/components/MapCard.jsx
+++ src/components/MapCard.jsx
@@ -4,6 +4,7 @@ import { formatDate, getImage } from "../utils/utils";
 import GlobalMapContext from "../contexts/map";
 import ConfirmDeleteModal from "./modals/ConfirmDeleteModal";
 import AuthContext from "../auth";
+import { useNavigate } from "react-router";
 
 function MapCard(props) {
   const { auth } = useContext(AuthContext)
@@ -15,6 +16,8 @@ function MapCard(props) {
   const [newTitle, setNewTitle] = useState(title);
   const dropdownRef = useRef(null)
   const imageUrl = thumbnail ? getImage(thumbnail.imageData) : '/'
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     document.addEventListener("mousedown", handleOutsideClick);
@@ -30,12 +33,14 @@ function MapCard(props) {
     }
   };
 
-  const handleRename = () => {
+  const handleRename = (e) => {
+    e.stopPropagation();
     setShowOptions(false);
     setIsEditing(true);
   };
 
   const handleTitleChange = (event) => {
+    event.stopPropagation();
     setNewTitle(event.target.value);
   };
 
@@ -46,32 +51,43 @@ function MapCard(props) {
     }
   };
 
-  const handleExport = () => {
+  const handleExport = (e) => {
+    e.stopPropagation();
     setShowOptions(false)
     map.exportMap(mapId, auth.user.userId)
   }
 
-  const handlePublish = () => {
+  const handlePublish = (e) => {
+    e.stopPropagation();
     setShowOptions(false)
     // TODO: go to edit post screen
   }
 
-  const handleFork = () => {
+  const handleFork = (e) => {
+    e.stopPropagation();
     setShowOptions(false)
     map.forkMap(mapId)
   }
 
-  const onDeleteClick = () => {
+  const onDeleteClick = (e) => {
+    e.stopPropagation();
     setShowOptions(false)
     setShowModal(true)
   }
 
-  const handleDelete = () => {
+  const handleDelete = (e) => {
+    e.stopPropagation();
     map.deleteMap(mapId)
+    setShowModal(false)
+  }
+
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    setShowOptions(!showOptions)
   }
 
   return (
-    <div className="mapCardWrapper">
+    <div className="mapCardWrapper" onClick={() => navigate('/editMap/' + mapId)}>
       <img src={imageUrl} alt='map' className="mapCardImagePreview"></img>
       <div className="mapCardDescription">
       <div className="mapCardInfo">
@@ -79,6 +95,7 @@ function MapCard(props) {
             <input
               className="mapCardTitleInput"
               value={newTitle}
+              onClick={(e) => e.stopPropagation()}
               onChange={handleTitleChange}
               onKeyDown={handleTitleSubmit}
               autoFocus
@@ -113,11 +130,14 @@ function MapCard(props) {
               </div>
             </div>
           )}
-          <span className="material-icons" onClick={() => setShowOptions(!showOptions)}>more_vert</span>
+          <span className="material-icons" onClick={toggleMenu}>more_vert</span>
         </div>
       </div>
       {showModal && (
-        <ConfirmDeleteModal onCancel={() => setShowModal(false)} onConfirm={handleDelete} />
+        <ConfirmDeleteModal onCancel={(e) => {
+          e.stopPropagation()
+          setShowModal(false)
+        }} onConfirm={handleDelete} />
       )}
     </div>
   );
