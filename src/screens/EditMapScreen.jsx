9f6b72cf82a2958b8c7923217aa71f9caaa8c0b6
us1aaron <aaron.li@stonybrook.edu>
2023-12-18 07:22:43 -0500

    timestamp and css fixes

--- src/screens/EditMapScreen.jsx
+++ src/screens/EditMapScreen.jsx
@@ -1,5 +1,5 @@
 import "../static/css/editMap/editMap.css";
-import Toolbox from '../components/map/Toolbox';
+import Toolbox from "../components/map/Toolbox";
 import GeoJSONMap from "../components/map/GeoJSONMap";
 
 import { useParams } from "react-router";
@@ -10,59 +10,71 @@ import Legend from "../components/map/Legend";
 // import GeoJSONMapPureLeaflet from "../components/map/GeoJSONMapPureLeaflet";
 
 //https://stackoverflow.com/questions/66704970/take-a-screenshot-of-leaflet-map
-const EditMapScreen = () =>{
-    const {id} = useParams()
-    const {post} = useContext(GlobalPostContext)
-    const {map} = useContext(GlobalMapContext)
-    const [mapRef, setMapRef] = useState(null)
+const EditMapScreen = () => {
+  const { id } = useParams();
+  const { post } = useContext(GlobalPostContext);
+  const { map } = useContext(GlobalMapContext);
+  const [mapRef, setMapRef] = useState(null);
 
-    // const [featureGroupRef, setFeatureGroupRef] = useState(null)
-    const featureGroupRef = useRef(null)
+  // const [featureGroupRef, setFeatureGroupRef] = useState(null)
+  const featureGroupRef = useRef(null);
 
-    useEffect(() => {
-        post.exitCurrentPost()
-      }, [])
+  useEffect(() => {
+    post.exitCurrentPost();
+  }, []);
 
-
-    const handleUndo = (e) => {
-        e.preventDefault()
-        if (!map.canUndo()) {
-            console.log("cant undo")
-            return 
-        }
-        map.undo()
+  const handleUndo = (e) => {
+    e.preventDefault();
+    if (!map.canUndo()) {
+      console.log("cant undo");
+      return;
     }
+    map.undo();
+  };
 
-    const handleRedo = (e) => {
-        e.preventDefault()
-        if (!map.canRedo()) {
-            console.log("cant redo")
-            return 
-        }
-        map.redo()
-
+  const handleRedo = (e) => {
+    e.preventDefault();
+    if (!map.canRedo()) {
+      console.log("cant redo");
+      return;
     }
-    return (
-        <div className="editMapWrapper">
-            <div className="mapScreen">
-                {/* <GeoJSONMapPureLeaflet  className="mapOverlay" position={[39.74739, -105]} setMapRef={setMapRef} mapRef={mapRef} mapMetadataId={id} editEnabled={true} width="100vw" height="100vh"/>  */}
-                <GeoJSONMap className="mapOverlay" position={[39.74739, -105]} setMapRef={setMapRef} mapRef={mapRef} mapMetadataId={id} 
-                editEnabled={true}
-                featureGroupRef={featureGroupRef}
-                width="100vw" height="94vh"
-                />
-                <div className="rightPanel">
-                    <Toolbox mapId={id} mapRef={mapRef} featureGroupRef={featureGroupRef}/>
-                    <Legend />
-                    <div className="sideControls">
-                    <div className="iconGroup">
-                        <span className="material-icons" onClick={handleUndo} >undo</span>
-                        <span className="material-icons" onClick={handleRedo} >redo</span>
-                    </div>
-                    </div>
-                </div>
-            </div>
+    map.redo();
+  };
+  return (
+    <div className="editMapWrapper">
+      <div className="mapScreen">
+        {/* <GeoJSONMapPureLeaflet  className="mapOverlay" position={[39.74739, -105]} setMapRef={setMapRef} mapRef={mapRef} mapMetadataId={id} editEnabled={true} width="100vw" height="100vh"/>  */}
+        <GeoJSONMap
+          className="mapOverlay"
+          position={[39.74739, -105]}
+          setMapRef={setMapRef}
+          mapRef={mapRef}
+          mapMetadataId={id}
+          editEnabled={true}
+          featureGroupRef={featureGroupRef}
+          width="100vw"
+          height="94vh"
+        />
+        <div className="rightPanel">
+          <Toolbox
+            mapId={id}
+            mapRef={mapRef}
+            featureGroupRef={featureGroupRef}
+          />
         </div>
-    );
-}
+        <Legend />
+        <div className="sideControls">
+          <div className="iconGroup">
+            <span className={`material-icons ${((!map.canUndo())) ? ('disabled') : ('')}`} onClick={handleUndo}>
+              undo
+            </span>
+            <span className={`material-icons ${((!map.canRedo())) ? ('disabled') : ('')}`} onClick={handleRedo}>
+              redo
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default EditMapScreen;
